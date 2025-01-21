2da7966f5ca9ad95658604f9d4f6e06c22bdd384
pauleye <orepaul12@gmail.com>
2025-01-21 10:53:40 +0100

    fixed build error

--- app/edit-task/[id]/page.tsx
+++ app/edit-task/[id]/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import React from "react";
-import { useRouter, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import TaskForm from "@/components/TaskForm";
-import { Button } from "@/components/ui/button";
+
 import { useTaskContext } from "@/components/TaskContext";
+import Link from "next/link";
 
 export default function EditTaskPage() {
-  const router = useRouter();
+  // const router = useRouter();
   const params = useParams();
   const { tasks } = useTaskContext();
 
   const taskId = params.id as string;
   const task = tasks.find((t) => t.id === taskId);
 
-  const handleClose = () => {
-    router.push("/");
-  };
+  // const handleClose = () => {
+  //   router.push("/");
+  // };
 
   if (!task) {
     return <div>Task not found</div>;
@@ -27,9 +28,12 @@ export default function EditTaskPage() {
       <div className="max-w-2xl mx-auto">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-3xl font-bold">Edit Task</h1>
-          <Button onClick={handleClose} variant="outline">
+          <Link
+            className="text-white bg-indigo-600 rounded-lg px-4 py-2 "
+            href={"/"}
+          >
             Back to Calendar
-          </Button>
+          </Link>
         </div>
         <TaskForm task={task} />
       </div>
